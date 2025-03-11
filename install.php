e9461f1bc80f6ba9bb7a792201fea71611b0dd19
TaimoorQadri123 <dchota374@gmail.com>
2025-03-11 10:32:13 +0500

    first task yahoo.php done

---
+++ install.php
@@ -0,0 +1,51 @@
+$<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+   
+  <div class="container">
+    <form action="" method = "post">
+      
+     <div class="form-group">
+       <label for="">Name</label>
+       <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+     </div>
+     <div class="form-group">
+       <label for="">Age</label>
+       <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+     </div>
+     <div class="form-group">
+       <label for="">Salary</label>
+       <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+     </div>
+     <div class="form-group">
+       <label for="">loan Amount</label>
+       <select class="form-control" name="" id="">
+         <option>200000</option>
+         <option>300000</option>
+         <option>500000</option>
+       </select>
+     </div>
+     <div class="form-group">
+       <label for="">installment duration</label>
+       <select class="form-control" name="" id="">
+         <option value= "2">2 years</option>
+         <option value= "3">3 years</option>
+         <option value= "4">4 years</option>
+       </select>
+     </div>
+     <button class="btn btn-info" name="sub"  >submit</button>
+    </form>
+  </div>
+  
+
+  </body>
+</html>
