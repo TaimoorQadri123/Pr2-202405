3059a38d377da21c7262f7e5261c98adb913bb03
TaimoorQadri123 <dchota374@gmail.com>
2025-04-05 12:47:45 +0500

    current database

--- AdminPanel/addCategory.php
+++ AdminPanel/addCategory.php
@@ -1,9 +1,9 @@
 <?php
 include("php/query.php");
 include("components/header.php");
-// if(!isset($_SESSION['adminEmail'])){
-//     echo "<script>location.assign('../login.php')</script>";
-//     }
+if(!isset($_SESSION['adminEmail'])){
+    echo "<script>location.assign('../login.php')</script>";
+    }
 ?>
 
 
